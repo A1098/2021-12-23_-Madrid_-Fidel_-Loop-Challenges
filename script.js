909cd8b5dc1c5ad52ca51bff179e3303acad503f
John Doe <johndoe@example.com>
2021-12-24 11:36:07 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var x = "0"
+
+for (var i=1; i<21; i+=2) {
+    x += i;
+        console.log(i);
+}
+var y = "0"
+
+for (var i=100; i>0; i--) {
+    y -= i;
+    if (y = i % 3 == 0){
+        console.log(i);
+    }
+}
+for (var i=4; i>-3.6; i-=1.5) {
+    console.log(i)
+}
+var x = 1;
+var y = 0;
+
+    for (var i=0; i<101; i++){
+        var y = y += i;
+        console.log(y);
+    }
+    var x = 1;
+    var y = 1;
+    
+        for (var i=2; i<13; i++){
+            var y = y * i;
+            console.log(y);
+        }
